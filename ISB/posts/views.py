2e27fdd95a6d70c1ad50cacf9bad2a1d531b470d
Rajat Mangal <rmangal@sfu.ca>
2020-02-05 21:33:57 -0800

    decorator and form model for new user added

---
+++ ISB/posts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render,redirect
+from django.forms import inlineformset_factory
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
+from .forms import CreateUserForm
+from .decorators import unauthenticated_user
