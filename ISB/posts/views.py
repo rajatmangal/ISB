880629d1f0b356fd4034f9e96954b52234c27f81
Rajat Mangal <rmangal@sfu.ca>
2020-02-05 22:14:02 -0800

    Standard Login, Register and Dashboard page added

--- ISB/posts/views.py
+++ ISB/posts/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render
 
-# Create your views here.
 from django.shortcuts import render,redirect
-from django.forms import inlineformset_factory
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
@@ -11,4 +8,48 @@ from .forms import CreateUserForm
 from .decorators import unauthenticated_user
 
 
+#@unauthenticated_user
+def registerPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
 
+    context={'form': form}
+    return render(request, 'posts/register.html', context)
+
+
+#@unauthenticated_user
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username') ##getting from names in login page
+        password = request.POST.get('password')
+        user = authenticate(request, username = username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect.')
+    context= {}
+    return render(request, 'posts/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+#@login_required(login_url='login')
+def home(request):
+    context = {}
+    return render(request, 'posts/dashboard.html', context)
+
+
+#@login_required(login_url='login')
+def userPage(request):
+    context={}
+    return render(request, 'posts/dashboard.html', context)
